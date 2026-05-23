53d1a96d1c6297b47fcdf554dc25820eeae276a5
rafapereirabr <rafa.pereira.br@gmail.com>
2026-05-23 13:42:04 -0300

    munis_bbox with duckdb parquet

--- tests/tests_rafa/munis_bbox.R
+++ tests/tests_rafa/munis_bbox.R
@@ -8,34 +8,34 @@ library(geoarrow)
 # Load the state polygons
 df <- geobr::read_municipality(year = 2022, simplified = T)
 
-# Calculate bounding boxes of states
-bounding_boxes <- df |>
-  st_as_sf() |>                           # Ensure df is an sf object
-  rowwise() |>                            # Process each polygon individually
-  mutate(
-    xmin = st_bbox(geometry)["xmin"],      # Extract xmin from the bounding box
-    ymin = st_bbox(geometry)["ymin"],      # Extract ymin from the bounding box
-    xmax = st_bbox(geometry)["xmax"],      # Extract xmax from the bounding box
-    ymax = st_bbox(geometry)["ymax"]       # Extract ymax from the bounding box
-  ) |>
-  ungroup() |>                            # Unrowwise after rowwise operations
-  select(code_muni, xmin, ymin, xmax, ymax) |> # Select desired columns
-  st_drop_geometry()
-
-# View the resulting bounding box data.frame
-head(bounding_boxes)
-
-data.table::fwrite(bounding_boxes, './inst/extdata/munis_bbox.csv')
-
-
-head(input_table)
-
-candidate_states <-
-  subset(x = bounding_boxes,
-         (xmin < bbox_lon_min | xmax > bbox_lon_max) &
-           (ymin < bbox_lat_min | ymax > bbox_lat_max)
-  )
-
+# # Calculate bounding boxes of states
+# bounding_boxes <- df |>
+#   st_as_sf() |>                           # Ensure df is an sf object
+#   rowwise() |>                            # Process each polygon individually
+#   mutate(
+#     xmin = st_bbox(geometry)["xmin"],      # Extract xmin from the bounding box
+#     ymin = st_bbox(geometry)["ymin"],      # Extract ymin from the bounding box
+#     xmax = st_bbox(geometry)["xmax"],      # Extract xmax from the bounding box
+#     ymax = st_bbox(geometry)["ymax"]       # Extract ymax from the bounding box
+#   ) |>
+#   ungroup() |>                            # Unrowwise after rowwise operations
+#   select(code_muni, xmin, ymin, xmax, ymax) |> # Select desired columns
+#   st_drop_geometry()
+#
+# # View the resulting bounding box data.frame
+# head(bounding_boxes)
+#
+# data.table::fwrite(bounding_boxes, './inst/extdata/munis_bbox.csv')
+#
+#
+# head(input_table)
+#
+# candidate_states <-
+#   subset(x = bounding_boxes,
+#          (xmin < bbox_lon_min | xmax > bbox_lon_max) &
+#            (ymin < bbox_lat_min | ymax > bbox_lat_max)
+#   )
+#
 
 
 
@@ -65,10 +65,24 @@ bounding_boxes <- bounding_boxes |>
 
 head(bounding_boxes)
 
-# arrow::write_parquet(bounding_boxes2, "munis_bbox_2022.parquet")
-arrow::write_parquet(bounding_boxes, "munis_bbox_2022.parquet",
-                     compression='zstd',
-                     compression_level = 7)
+# # arrow::write_parquet(bounding_boxes2, "munis_bbox_2022.parquet")
+# arrow::write_parquet(bounding_boxes, "munis_bbox_2022.parquet",
+#                      compression='zstd',
+#                      compression_level = 7)
+#
+
+# remove the classes "tbl_df" "tbl" from an object
+class(bounding_boxes) <- setdiff(class(bounding_boxes), c("tbl_df", "tbl"))
+
+duckspatial::ddbs_write_dataset(
+  data = bounding_boxes,
+  path = './inst/extdata/munis_bbox_2022.parquet',
+  crs = "EPSG:4674",
+  overwrite = T,
+  parquet_compression = "ZSTD",
+  quiet = TRUE
+)
+
 
 
 path <- "//storage6/usuarios/Proj_acess_oport/git_rafa/prep_roger/data/municipality/2022/municipalities_2022_simplified.parquet"
